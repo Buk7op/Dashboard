be636a7e47e0461e589e40b6080a5cca0078cc2f
Buk7op <robxviktor@gmail.com>
2022-08-09 06:38:04 +0300

    changed navbar representation

--- src/app/views/categories/categories.component.ts
+++ src/app/views/categories/categories.component.ts
@@ -12,6 +12,7 @@ import { DataHandlerService } from 'src/app/services/data-handler.service';
 export class CategoriesComponent implements OnInit {
 
   categories!: Category[];
+  selectedCategory?: Category;
 
   constructor(private dataHandler: DataHandlerService) { }
 
@@ -20,6 +21,7 @@ export class CategoriesComponent implements OnInit {
   }
 
   showTaskByCategory(category: Category) {
+    this.selectedCategory = category;
     this.dataHandler.fillTasksByCategory(category);
   }
 
