e2ccccc8d258b9b625783ae8d481a3de2be20681
Buk7op <robxviktor@gmail.com>
2022-08-12 07:26:24 +0300

    added done and operations button

--- src/app/views/tasks/tasks.component.ts
+++ src/app/views/tasks/tasks.component.ts
@@ -5,6 +5,7 @@ import { MatSort } from "@angular/material/sort";
 import { EditTaskDialogComponent } from "../../dialog/edit-task-dialog/edit-task-dialog.component";
 import { MatDialog } from "@angular/material/dialog";
 import { MatTableDataSource } from '@angular/material/table';
+import { ConfirmDialogComponent } from 'src/app/dialog/confirm-dialog/confirm-dialog.component';
 
 const COMPLETED_COLOR = '#F8F9FA';
 const NO_COLOR = '#FFF'
@@ -17,8 +18,8 @@ const NO_COLOR = '#FFF'
 export class TasksComponent implements OnInit {
 
 
-  displayedColumns: string[] = ['color', 'id', 'title', 'date', 'priority', 'category'];
-  dataSource!: MatTableDataSource<Task>; 
+  displayedColumns: string[] = ['color', 'id', 'title', 'date', 'priority', 'category', 'select', 'operations'];
+  dataSource!: MatTableDataSource<Task>;
 
   @ViewChild(MatPaginator, { static: false }) paginator!: MatPaginator;
   @ViewChild(MatSort, { static: false }) sort!: MatSort;
@@ -30,7 +31,7 @@ export class TasksComponent implements OnInit {
 
   tasks!: Task[];
 
-  
+
   @Input('tasks')
   set setTasks(tasks: Task[]) {
     this.tasks = tasks;
@@ -42,34 +43,35 @@ export class TasksComponent implements OnInit {
 
   ngOnInit() {
     this.dataSource = new MatTableDataSource();
-    this.fillTable(); 
+    this.fillTable();
   }
 
 
-  toggleTaskCompleted(task: Task) {
+  onToggleStatus(task: Task) {
     task.completed = !task.completed;
+    this.updateTask.emit(task);
   }
 
-  
+
   getPriorityColor(task: Task): string {
     if (task.completed) {
-      return COMPLETED_COLOR; 
+      return COMPLETED_COLOR;
     }
 
     if (task.priority && task.priority.color) {
       return task.priority.color;
     }
-    return NO_COLOR; 
+    return NO_COLOR;
   }
 
-  
+
   private fillTable(): void {
 
     if (!this.dataSource) {
       return;
     }
 
-    this.dataSource.data = this.tasks; 
+    this.dataSource.data = this.tasks;
 
     this.addTableObjects();
 
@@ -97,9 +99,9 @@ export class TasksComponent implements OnInit {
 
     };
   }
-  
-  openEditTaskDialog(task:Task): void {
-    const dialogRef = this.dialog.open(EditTaskDialogComponent,{data: [task, 'Edit task'], autoFocus: false});
+
+  openEditTaskDialog(task: Task): void {
+    const dialogRef = this.dialog.open(EditTaskDialogComponent, { data: [task, 'Edit task'], autoFocus: false });
     dialogRef.afterClosed().subscribe(result => {
       switch (result) {
         case "delete":
@@ -116,13 +118,21 @@ export class TasksComponent implements OnInit {
         case result as Task:
           this.updateTask.emit(result);
           break;
-        }
-      // if(result as Task) {
-      //   this.updateTask.emit(result);
-      //   return;
-      // }
+      }
     });
-    
+  }
+
+  openDeleteTaskDialog(task: Task): void {
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      maxWidth: '500px',
+      data: { dialogTitle: 'Confirm the action', message: `Do you really want to delete the task: "${task.title}"?` },
+      autoFocus: false
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.deleteTask.emit(task);
+      }
+    });
   }
 
   onClickTask(task: Task) {
@@ -130,8 +140,8 @@ export class TasksComponent implements OnInit {
   }
 
   private addTableObjects(): void {
-    this.dataSource.sort = this.sort; 
-    this.dataSource.paginator = this.paginator; 
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
   }
 
 }
