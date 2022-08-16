b68575a2a3f0d281614be282a4029f01093c989c
Buk7op <robxviktor@gmail.com>
2022-08-16 13:50:35 +0300

    fix issue with deleting in all category

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,80 +1,102 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from './model/Task';
+import { Category } from "./model/Category";
+import { Priority } from "./model/Priority";
 import { DataHandlerService } from './services/data-handler.service';
-import { Task } from "src/app/model/Task";
-import { Category } from './model/Category';
-import { Priority } from './model/Priority';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html',
+  styles: []
 })
-export class AppComponent implements OnInit{
-  title = 'Todo-App';
+export class AppComponent implements OnInit {
+  private title = 'Todo';
+  private searchTaskText = '';
+  private priorityFilter: Priority;
+  private statusFilter: boolean;
   tasks: Task[];
   categories: Category[];
   priorities: Priority[];
-  selectedCategory: Category;
-  selectedTask?: Task;
-  statusFilter: boolean;
-  titleFilter: string;
-  priorityFilter: Priority;
+  selectedCategory: Category = null!;
 
-  constructor(private dataHandler: DataHandlerService) {
-    
+  constructor(
+    private dataHandler: DataHandlerService,
+  ) {
   }
 
   ngOnInit(): void {
-    this.dataHandler.getAllTasks().subscribe(tasks => this.tasks = tasks);
-    this.dataHandler.getAllCategories().subscribe(category => this.categories = category);
+    this.dataHandler.getAllPriorities().subscribe(priorities => this.priorities = priorities);
+    this.dataHandler.getAllCategories().subscribe(categories => this.categories = categories);
+
+    this.onSelectCategory(null!);
+
   }
 
   onSelectCategory(category: Category) {
+
     this.selectedCategory = category;
-    this.dataHandler.searchTasks(category, null!, null!, null!).subscribe(tasks => this.tasks = tasks);
+
+    this.dataHandler.searchTasks(
+      this.selectedCategory,
+      null!,
+      null!,
+      null!
+    ).subscribe(tasks => {
+      this.tasks = tasks;
+    });
+
   }
 
   onUpdateTask(task: Task) {
-    this.dataHandler.updateTask(task).subscribe(() => {
-      this.dataHandler.searchTasks(
-        this.selectedCategory, null!, null!, null!
-      ).subscribe(t => this.tasks = t)
-    });
+
+    this.updateTasks();
+
   }
 
   onDeleteTask(task: Task) {
-    this.dataHandler.deleteTask(task).subscribe(() => {
-      this.dataHandler.searchTasks(
-        this.selectedCategory, null!, null!, null!
-      ).subscribe(t => this.tasks = t)
+
+    this.dataHandler.deleteTask(task).subscribe(cat => {
+      this.updateTasks()
     });
   }
 
   onUpdateCategory(category: Category) {
     this.dataHandler.updateCategory(category).subscribe(() => {
       this.onSelectCategory(this.selectedCategory);
-    })
+    });
   }
 
   onDeleteCategory(category: Category) {
     this.dataHandler.deleteCategory(category).subscribe(cat => {
       this.selectedCategory = null!;
       this.onSelectCategory(this.selectedCategory);
-    })
+    });
+  }
+
+  onFilterTaskByTitle(searchString: string) {
+    this.searchTaskText = searchString;
+    this.updateTasks();
   }
 
   onFilterTaskByStatus(status: boolean) {
     this.statusFilter = status;
-    this.dataHandler.searchTasks(null!,null!,status,null!).subscribe(tasks => this.tasks = tasks);
-  }
-
-  onFilterTaskByTitle(title: string) {
-    this.titleFilter = title;
-    this.dataHandler.searchTasks(null!,title,null!,null!).subscribe(tasks => this.tasks = tasks);
+    this.updateTasks();
   }
 
   onFilterTaskByPriority(priority: Priority) {
     this.priorityFilter = priority;
-    this.dataHandler.searchTasks(null!,null!,null!,priority).subscribe(tasks => this.tasks = tasks);
+    this.updateTasks();
   }
+
+  updateTasks() {
+    this.dataHandler.searchTasks(
+      this.selectedCategory,
+      this.searchTaskText,
+      this.statusFilter,
+      this.priorityFilter
+    ).subscribe((tasks: Task[]) => {
+      this.tasks = tasks;
+    });
+  }
+
 }
